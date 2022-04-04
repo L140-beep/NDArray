d3d5809306f270ab26bd9e5ce310361ad0c5b534
L140-beep <70793854+L140-beep@users.noreply.github.com>
2022-04-04 18:38:11 +0800

    fix min() function

--- main.cpp
+++ main.cpp
@@ -220,7 +220,7 @@ public:
             NDArray new_arr({ this->size_y, 1 });
             new_arr.zeros();
             for (int i = 0; i < this->size_y; i++) {
-                min = this->arr[normalize_index(1, i)];;
+                min = this->arr[normalize_index(i, 1)];;
                 for (int j = 0; j < this->size_x; j++) {
                     if (this->arr[normalize_index(j, i)] < min) {
                         min = this->arr[normalize_index(i, j)];
